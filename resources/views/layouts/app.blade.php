d7df60c3b1d36595b92945e2dde009d30bac4cc4
abimon <eddyabby59@gmail.com>
2024-12-19 17:29:15 +0300

    colors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,7 +97,7 @@
                         </form>
                         <ul class="social_icon d-flex justify-content-center">
                             <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-x-twitter" aria-hidden="true"></i></a></li>
                             <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
                             <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
                         </ul>
