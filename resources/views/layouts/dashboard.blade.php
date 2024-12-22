2eeddba5bdb8f7ca6ae952cfa518ef7e7d8c45f2
abimon <eddyabby59@gmail.com>
2024-12-22 11:33:16 +0300

    contact

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -125,44 +125,7 @@
         <nav
           class="navbar navbar-header navbar-header-transparent navbar-expand-lg border-bottom">
           <div class="container-fluid">
-            <nav
-              class="navbar navbar-header-left navbar-expand-lg navbar-form nav-search p-0 d-none d-lg-flex">
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <button type="submit" class="btn btn-search pe-1">
-                    <i class="fa fa-search search-icon"></i>
-                  </button>
-                </div>
-                <input
-                  type="text"
-                  placeholder="Search ..."
-                  class="form-control" />
-              </div>
-            </nav>
-
             <ul class="navbar-nav topbar-nav ms-md-auto align-items-center">
-              <li
-                class="nav-item topbar-icon dropdown hidden-caret d-flex d-lg-none">
-                <a
-                  class="nav-link dropdown-toggle"
-                  data-bs-toggle="dropdown"
-                  href="#"
-                  role="button"
-                  aria-expanded="false"
-                  aria-haspopup="true">
-                  <i class="fa fa-search"></i>
-                </a>
-                <ul class="dropdown-menu dropdown-search animated fadeIn">
-                  <form class="navbar-left navbar-form nav-search">
-                    <div class="input-group">
-                      <input
-                        type="text"
-                        placeholder="Search ..."
-                        class="form-control" />
-                    </div>
-                  </form>
-                </ul>
-              </li>
               <li class="nav-item topbar-icon dropdown hidden-caret">
                 <a
                   class="nav-link dropdown-toggle"
@@ -183,7 +146,7 @@
                       Messages
                       <a href="/contact/index" class="small">Mark all as read</a>
                     </div>
-                  </li>
+                </li>
                   @foreach (App\Models\Contact::orderBy('created_at','asc')->take(3)->latest()->get() as $message)
                   <li>
                     <div class="message-notif-scroll scrollbar-outer">
@@ -360,7 +323,7 @@
                   aria-expanded="false">
                   <div class="avatar-sm">
                     <img
-                      src="/storage/assets/img/profile.jpg"
+                      src="/storage/assets/img/profile.png"
                       alt="..."
                       class="avatar-img rounded-circle" />
                   </div>
--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,8 @@ Route::get('/news/{article}', [ArticleController::class, 'show']);
 Route::controller(ContactController::class)->group(function () {
     Route::get('/contact/index', 'index');
     Route::post('/contactform', 'store');
-    Route::get('/contact/{contact}', 'show')->middleware('auth');
-    Route::put('/contact/{contact}', 'update');
-    Route::delete('/contact/{contact}', 'destroy');
+    Route::get('/contact/{contact}', 'show')->middleware(['auth',isAdmin::class]);
+    Route::delete('/contact/{contact}', 'destroy')->middleware(['auth',isAdmin::class]);
 });
 Route::middleware(['auth',isAdmin::class])->group(function () {
     Route::get('/dashboard', function () {
